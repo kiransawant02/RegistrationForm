586870df63845551719e208e09fa3d22a2ece62d
Kiran Sawant <sawantk389@gmail.com>
2023-07-30 19:11:06 +0530

    Add project files.

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using RegistrationForm.Models;
+using System.Web.Services.Description;
+
+namespace RegistrationForm.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Register rg)
+        {
+            MySqlConnection con = new MySqlConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+            string sqlquery = "Insert into UserReg (UserName, Age, Email, UserPassword, Phone, Gender, Status) values(@name,@age,@email,@pass,@phone,@gender,@status)";
+            MySqlCommand cmd = new MySqlCommand(sqlquery, con);
+            con.Open();
+            cmd.Parameters.AddWithValue("@name", rg.Username);
+            cmd.Parameters.AddWithValue("@age", rg.Age);
+            cmd.Parameters.AddWithValue("@email", rg.Email);
+            cmd.Parameters.AddWithValue("@pass", rg.Password);
+            cmd.Parameters.AddWithValue("@phone", rg.Phone);
+            cmd.Parameters.AddWithValue("@gender", rg.Gender);
+            cmd.Parameters.AddWithValue("@status", rg.Status);
+            cmd.ExecuteNonQuery();
+            ViewData["Message"] = "Record Inserted !!";
+            con.Close();
+            return View();            
+        }
+    }
+}
---
+++ Models/Register.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Xunit.Sdk;
+
+namespace RegistrationForm.Models
+{
+    public class Register
+    {
+        [Required]
+        [Display(Name = "Enter Username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Display(Name = "Enter Age")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Enter Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Enter Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Display(Name = "Enter Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+
+        [Required]
+        [Display(Name = "Enter your Gender")]
+        public string Gender { get; set; }
+        
+        [Required]
+        [Display(Name = "Enter marital status")]
+        public string Status { get; set; }
+
+    }
+}
